2df2505ecc0cd4a424e516d1103b6c52eb153994
mitchdrew <mitchdrew@goatse.cx>
2021-02-26 13:48:43 -0800

    master - mdrew: initial commit

---
+++ provider/provider.h
@@ -0,0 +1,18 @@
+//
+//  provider.h
+//  provider
+//
+//  Created by Mitchell Drew on 26/2/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for provider.
+FOUNDATION_EXPORT double providerVersionNumber;
+
+//! Project version string for provider.
+FOUNDATION_EXPORT const unsigned char providerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <provider/PublicHeader.h>
+
+
