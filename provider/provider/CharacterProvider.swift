8f67757b8578b6c89ba9c53caa2f2b8709dbaacd
mitchdrew <mitchdrew@goatse.cx>
2021-04-01 07:26:04 -0700

    master - mdrew: characterprovider renovated

--- provider/provider/CharacterProvider.swift
+++ provider/provider/CharacterProvider.swift
@@ -9,57 +9,113 @@ import Foundation
 import presenter
 
 open class CharacterProvider: ICharacterProvider {
+    
     private var listeners = [ICharacterProviderListener]()
     private var characters = [ModelCharacter]()
     private let restManager:IRestManager
     private let fileManager:IFileManager
-    private let serializer:CharacterSerializer
+    private let mapper:ICharacterMapper
+    private var requests = [[KotlinInt]]()
+    private var isDiskPulled = false
     private let url:URL
+    private let encoder:IEncoder
+    private let JSON_FILENAME = "chars.json"
     
-    public init(restManager:IRestManager, fileManager:IFileManager, serializer:CharacterSerializer, charUrl:URL){
+    public init(restManager:IRestManager, fileManager:IFileManager, serializer:ICharacterMapper, userDefaults:IUserDefaults, charUrl:URL, encoder:IEncoder){
         self.restManager = restManager
         self.fileManager = fileManager
-        self.serializer = serializer
+        self.mapper = serializer
         self.url = charUrl
+        self.encoder = encoder
     }
     
-    public func get(id: Int32) {
-        
-        restManager.dataTask(with: URLRequest(url: url), completionHandler: getRestClosure{}).resume()
-        /**
-         
-             let date = Date().currentTimeMillis()
-             getFromDisk{games in self.informListeners(games: games)}
-             if(isDatePastPullTimer(date:date)){
-                 lastGet = date
-                 restManager.dataTask(with: URLRequest(url: gamesUrl), completionHandler: getRestClosure{}).resume()
-             }
-         */
-    }
-    
-    private func getRestClosure(completionBlock: @escaping CompletionBlock) -> RestClosure {
-        return { [self] data,response,error in
-            if let uResponse = response as? HTTPURLResponse {
-                if(uResponse.statusCode != 200) { informListenersError(id: 0, error: KotlinException(message: "bad status: \(uResponse)")) }
-                if let uData = data {
-                    do { games = try serializer.map(data: uData)
-                        informListeners(games: games)
-                        saveToDisk(data: uData)
-                        completionBlock()
-                    }
-                    catch {
-                        informListenersError(id: 0, error: KotlinException(message: error.localizedDescription))
-                    }
+    public func get(ids: [KotlinInt]) {
+            if(!isDiskPulled){
+                pullFromDisk(ids: ids)
+            }
+            else{
+                if(isSatisfiable(request: ids)){ informListeners(ids: ids, chars: retrieve(ids: ids)) }
+                else{
+                    requests.append(ids)
+                    restManager.dataTask(with: createRequest(ids: ids), completionHandler: getRestClosure(ids: ids)).resume()
                 }
             }
-            else{informListenersUnexpectedError()}
+    }
+    
+    private func pullFromDisk(ids: [KotlinInt]){
+        isDiskPulled = true
+        requests.append(ids)
+        if let json = fileManager.contents(atPath: fileManager.currentDirectoryPath + JSON_FILENAME) {
+            do{
+                characters = try mapper.map(data: json)
+            }
+            catch {
+                informListenersError(ids: ids, error: KotlinException(message: "serialization error"))
+            }
+        }
+        restManager.dataTask(with: createRequest(ids: ids), completionHandler: getRestClosure(ids: ids)).resume()
+    }
+    
+    private func add(new:[ModelCharacter]){
+        for char in new {
+            characters.removeAll(where: {existingChar in return char.id == existingChar.id})
+            characters.append(char)
         }
     }
     
-    private func informListeners(chars: [ModelCharacter]){
-        for listener in listeners {
-            listener.onReceive(id: <#T##Int32#>, char: <#T##ModelCharacter#>)
+    private func getRestClosure(ids: [KotlinInt]) -> RestClosure {
+        return {[self] data, response, error in
+            if(isResponseGood(data:data, response:response, error:error)){
+                do{ add(new: try mapper.map(data: data!)) }
+                catch { informListenersError(ids: ids, error: KotlinException(message: error.localizedDescription)) }
+            }
+            else{
+                informListenersError(ids: ids, error: KotlinException(message: "error:\(error.debugDescription), response: \(response.debugDescription)"))
+            }
         }
+    }
+    
+    private func isResponseGood(data:Data?, response:URLResponse?, error:Error?) -> Bool {
+        return data != nil && (response as! HTTPURLResponse).statusCode == 200 && error == nil
+    }
+    
+    private func createRequest(ids: [KotlinInt]) -> URLRequest {
+        var ret = URLRequest(url: url)
+        var ids32 = [Int32]()
+        for id in ids{ ids32.append(id.int32Value) }
+        do{
+            try ret.httpBody = encoder.encode(ids32)
+        }
+        catch {
+            informListenersError(ids: ids, error: KotlinException(message: "request body encoding failed"))
+        }
+    }
+    
+    private func retrieve(ids: [KotlinInt]) -> [ModelCharacter] {
+        var ret = [ModelCharacter]()
+        for id in ids {
+            ret.append(characters.first(where: {char in return char.id == id.int32Value})!)
+        }
+        return ret
+    }
+    
+    private func informListeners(ids: [KotlinInt], chars: [ModelCharacter]){
+        for listener in listeners {
+            listener.onReceive(ids: ids, chars: chars)
+        }
+    }
+    
+    private func informListenersError(ids: [KotlinInt], error: KotlinException){
+        for listener in listeners {
+            listener.onError(ids: ids, error: error)
+        }
+    }
+    
+    private func isSatisfiable(request: [KotlinInt]) -> Bool {
+        for id in request {
+            if(!characters.contains(where: {char in return char.id == id.int32Value})){ return false }
+        }
+        return true
     }
     
     public func removeListener(charListener: ICharacterProviderListener) {
